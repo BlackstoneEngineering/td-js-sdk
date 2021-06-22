f658e781da7828d08347815a5e5f22d0c1630953
Austin Blackstone <austin.blackstone@TD-0250.local>
2021-06-23 04:49:42 +0900

    DOCS - migrated consent manager to its own file

--- build-docs.sh
+++ build-docs.sh
@@ -1,7 +1,32 @@
 #! bash
 cd lib
-jsdoc2md --configure ../jsdoc-config.json --files "." --no-cache --separators --example-lang js --partial ../scope.hbs --partial ../summary.hbs --partial ../global-index.hbs > ../js-sdk-api.md
+jsdoc2md \
+	--configure ../jsdoc-config.json \
+	--files "." \
+	--no-cache \
+	--separators \
+	--example-lang js \
+	--partial ../scope.hbs \
+	--partial ../summary.hbs \
+	--partial ../global-index.hbs \
+	> ../js-sdk-api.md
+
+jsdoc2md \
+	--configure ../jsdoc-config-cm.json \
+	--files "." \
+	--no-cache \
+	--separators \
+	--example-lang js \
+	--partial ../scope.hbs \
+	--partial ../summary.hbs \
+	--partial ../global-index.hbs \
+	> ../js-sdk-api_consentManager.md
+
+
+
 
 # local helper, delete before committing
 rm ~/code/TD-API-Documentation/docs/js-sdk/api.md
 cp ~/code/td-js-sdk/js-sdk-api.md ~/code/TD-API-Documentation/docs/js-sdk/api.md
+rm ~/code/TD-API-Documentation/docs/js-sdk/consent_manager.md
+cp ~/code/td-js-sdk/js-sdk-api_consentManager.md ~/code/TD-API-Documentation/docs/js-sdk/consent_manager.md
